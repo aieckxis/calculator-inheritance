b6eb882dc496da0ff26f7fa3e67b8d20381ad3a9
aieckxis <aleckxiskate@gmail.com>
2023-06-03 14:09:54 +0800

    asked the user to choose math operation continuously, didn't proceed asking for 2 numbers

--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,4 @@
-# Define MathOperations class
+# Define UserInterface class
 class UserInterface:
     def __init__(self):
         self.operation = ""
@@ -16,5 +16,5 @@ class UserInterface:
                 self.num1 = float(input("Enter the first number: "))
                 self.num2 = float(input("Enter the second number: "))
                 break
-        except ValueError:
-            print("Error: Invalid input. Please enter a number.")
+            except ValueError:
+                print("Error: Invalid input. Please enter a number.")
